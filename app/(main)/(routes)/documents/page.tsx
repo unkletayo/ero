8636f2908728e9f3ef1630e8c7a71e0abee57c37
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2023-12-07 23:42:18 +0100

    Enable Providers

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useUser } from "@clerk/clerk-react";
-import { PlusCircle } from "lucide-react";
-import { useMutation } from "convex/react";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { useUser } from '@clerk/clerk-react';
+import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
-import { api } from "@/convex/_generated/api";
-import { Button } from "@/components/ui/button";
+import { api } from '@/convex/_generated/api';
+import { Button } from '@/components/ui/button';
 
 const DocumentsPage = () => {
   const router = useRouter();
@@ -16,17 +16,18 @@ const DocumentsPage = () => {
   const create = useMutation(api.documents.create);
 
   const onCreate = () => {
-    const promise = create({ title: "Untitled" })
-      .then((documentId) => router.push(`/documents/${documentId}`))
+    const promise = create({ title: 'Untitled' }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
     toast.promise(promise, {
-      loading: "Creating a new note...",
-      success: "New note created!",
-      error: "Failed to create a new note."
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note.',
     });
   };
 
-  return ( 
+  return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
         src="/empty.png"
@@ -43,14 +44,14 @@ const DocumentsPage = () => {
         className="hidden dark:block"
       />
       <h2 className="text-lg font-medium">
-        Welcome to {user?.firstName}&apos;s Jotion
+        Welcome to {user?.firstName}&apos;s Ero
       </h2>
       <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DocumentsPage;
